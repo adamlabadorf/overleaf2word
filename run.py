93c2e479b94198818448aff86500a1d05252abf4
Adam <alabadorf@gmail.com>
2017-09-11 21:39:45 +0000

    added sources command line argument

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+import argparse
 import json
 from overleaf2word import overleaf2word
 
+parser = argparse.ArgumentParser(description='Convert overleaf latex sources to word docs')
+parser.add_argument('sources', help='path to JSON file containing overleaf sources',
+  nargs='?',default='sources.json'
+)
+
 if __name__ == '__main__':
-    with open('sources.json') as f :
+    args = parser.parse_args()
+    with open(args.sources) as f :
         overleaf_repos = json.load(f)
     for repo in overleaf_repos :
         overleaf2word(repo['git_clone_url'],repo.get('latex_paths',[]))
